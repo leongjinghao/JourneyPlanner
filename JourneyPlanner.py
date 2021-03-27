5456d2e42d1e673f0b5c6c9b1140cd57faba6f17
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2021-03-27 14:22:38 +0800

    -

--- JourneyPlanner.py
+++ JourneyPlanner.py
@@ -5,7 +5,12 @@ from EdgeWeightedDigraph import EdgeWeightedDigraph
 #print(mrt.shortest_path("EW28", "NE14"))
 
 mrt = EdgeWeightedDigraph("mrt_stations_weighted.txt")
+'''
 allEdges = mrt.getAllEdges()
 print(allEdges)
 for edge in allEdges:
     print(edge.toString())
+'''
+arr = []
+arr.insert("a", 0.0)
+print(arr)
