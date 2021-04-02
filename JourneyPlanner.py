872921e2f71b8d77b724bedbb09caf4e14f75408
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2021-04-02 19:51:00 +0800

    comments for PriorityQueue class

--- JourneyPlanner.py
+++ JourneyPlanner.py
@@ -1,20 +1,7 @@
 from EdgeWeightedGraph import EdgeWeightedGraph
-from queue import PriorityQueue
 from DijkstraSP import DijkstraSP
 
 mrt = EdgeWeightedGraph("mrt_stations_weighted_1.csv")
-'''
-allEdges = mrt.getAllEdges()
-#print(allEdges)
-for edge in allEdges:
-    print(edge.toString())
-
-print(len(edgeTo))
-'''
-edges = mrt.adjList[80]
-for edge in edges:
-    print(edge.vertex, edge.desVertex, edge.weight)
-
 
 route = []
 path = DijkstraSP(mrt, mrt.allNodesIndex["NS13"])
@@ -28,9 +15,3 @@ while (dest_stn != mrt.allNodesIndex["NS13"]):
 while len(route) != 0:
     # print route in the reverse order
     print(route.pop())
-
-#print(mrt.allNodesIndex)
-#print(len(edgeTo))
-
-#for line in edgeTo:
-#    print(line)
