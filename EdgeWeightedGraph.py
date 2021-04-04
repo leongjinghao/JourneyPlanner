cb17bb9f77148b3d77788315ecc25868482b0ae4
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2021-04-04 22:48:02 +0800

    more comments

--- EdgeWeightedGraph.py
+++ EdgeWeightedGraph.py
@@ -9,41 +9,61 @@ class EdgeWeightedGraph:
         self.adjList = {}
         self.allNodesIndex = {}
 
+        # read from a file input
         text_file = open(fileInput, "r")
+        # list of each line on the file input
         lines = text_file.readlines()
+        # method to index all vertices
         self.setAllNodesIndex()
+
+        # for each line
         for line in lines:
-            #remove line breaks, which is the last char
+            #remove line break, which is the last char
             line = line[:-1]
+            # split vertex and destination vertex
             vertex, desVertices = line.split(": ")
+            # might have multiple destination vertices, split by "," if so
             desVerticesWithWeight = desVertices.split(", ")
+            # for each destination vertices, of a vertex
             for vertexWithWeight in desVerticesWithWeight:
+                # split the destination vertex and weight
                 desVertex, weight = vertexWithWeight.split(" - ")
+                # add the edge (destination vertex and its weight) of the vertex into the adjList
                 self.addEdge(self.allNodesIndex[vertex], self.allNodesIndex[desVertex], int(weight))
     
+    # method to add edge into the adjList
     def addEdge(self, vertex, desVertex, weight):
+        # initialise a directed edge, with the given input
         edge = DirectedEdge(vertex, desVertex, weight)
 
+        # if vertex is already inside the adjList, append edge into the list within the adjList of the vertex
         if vertex in self.adjList:
             self.adjList[vertex].append(edge)
+        # else create a new entry for vertex in the adjList and insert a new list [], append the edge into list
         else:
             self.adjList[vertex] = []
             self.adjList[vertex].append(edge)
         
+        # initialise edge for the other direction, from destination vertex to current vertex
         oppEdge = DirectedEdge(desVertex, vertex, weight)
 
+        # if desVertex is already inside the adjList, append edge into the list within the adjList of desVertex
         if desVertex in self.adjList:
             self.adjList[desVertex].append(oppEdge)
+        # else create a new entry for desVertex in the adjList and insert a new list [], append the edge into list
         else:
             self.adjList[desVertex] = []
             self.adjList[desVertex].append(oppEdge)
 
+    # method to retrieve the adjacent edges (list) of a vertex
     def getAdjacent(self, vertex):
         return self.adjList[vertex]
 
+    # method to retrieve all vertices
     def getAllVertices(self):
         return self.adjList.keys()
 
+    # method to retrieve all edges
     def getAllEdges(self):
         allEdges = []
         
@@ -53,18 +73,28 @@ class EdgeWeightedGraph:
 
         return allEdges
 
+    # method to index all the vertices (nodes)
     def setAllNodesIndex(self):
+        # read from a input file which stores all the unique stations code
         text_file = open("all_unique_stations.csv", "r")
         lines = text_file.readlines()
+        # index starts from 0
         index = 0
+        # for each station stored in the input file, give it an index and store the value inside allNodeIndex (dict)
         for line in lines:
+            # remove last char which is a line break
             node = line[:-1]
+            # insert the index value for the vertex (node) inside allNodeIndex
             self.allNodesIndex[node] = index
+            # increment index value for next index
             index += 1
 
+    # method to retrieve the station name from a given index
     def getStationName(self, index):
+        # traverse the allNodesIndex (dict) to find a match with the given index value
         for key, value in self.allNodesIndex.items():
             if value == index:
                 return key
         
+        # if no match found
         return "key does not exist"
--- JourneyPlanner.py
+++ JourneyPlanner.py
@@ -2,22 +2,36 @@ from EdgeWeightedGraph import EdgeWeightedGraph
 from DijkstraSP import DijkstraSP
 
 def computeRoute(currentLocation, destination):
+    # initialise the edge weighted graph
     mrt = EdgeWeightedGraph("mrt_stations_weighted.csv")
 
     route = []
     routeReverse = []
+
+    # search for the shortest path to all location from current location using dijkstra algo
     path = DijkstraSP(mrt, mrt.allNodesIndex[currentLocation])
+    # retrieve the index for the destination location (station)
     dest_stn = mrt.allNodesIndex[destination]
+    # station pointer used to traverse the path
+    stn_ptr = dest_stn
+    # retrieving the route by traversing the destination first and subsequently to current location
+    # hence append destination (station) first
     routeReverse.append(mrt.getStationName(dest_stn))
 
-    while (dest_stn != mrt.allNodesIndex[currentLocation]):
-        dest_stn = path.edgeTo[dest_stn].vertex
-        routeReverse.append(mrt.getStationName(dest_stn))
+    # while the station pointer is not at current lcation (station), continue traversing
+    while (stn_ptr != mrt.allNodesIndex[currentLocation]):
+        # set station pointer to the previous station
+        stn_ptr = path.edgeTo[stn_ptr].vertex
+        # append the station pointer to the reverse route list
+        routeReverse.append(mrt.getStationName(stn_ptr))
 
+    # while reverse route is not empty
     while len(routeReverse) != 0:
-        # print route in the reverse order
+        # append to route in the reverse order using stack pop property
         route.append(routeReverse.pop())
 
-    timeToDest = path.timeTo[mrt.allNodesIndex[destination]]
-    print("Time to {0}: {1} min".format(destination, path.timeTo[mrt.allNodesIndex[destination]]))
+    # retrieve the estimated time to the destination location (station)
+    timeToDest = path.timeTo[dest_stn]
+    # check
+    # print("Time to {0}: {1} min".format(destination, path.timeTo[mrt.allNodesIndex[destination]]))
     return [route, timeToDest]
