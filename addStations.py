6d16aa54c8814045d963a8b13ecd667adc5674e6
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2021-04-06 15:26:54 +0800

    addStation app

--- addStations.py
+++ addStations.py
@@ -4,8 +4,14 @@ import csv
 
 class GUI:
     background = 'lightgrey'
-    mainGUI = Tk(className='Journey Planner')  # Sets window name
+    mainGUI = Tk(className='Add Station App')  # Sets window name
     xyCoords = []
+    newStationName = StringVar()
+    newStationCode = StringVar()
+    nextStationCode = StringVar()
+    newStations = []
+    newStationCircle = [None] * 100 # display a max of 100 added point on gui
+    newStationCircleCounter = 0
 
     def __init__(self):
         self.guiInstance(self.mainGUI)
@@ -24,13 +30,29 @@ class GUI:
         instructions.config(font=("Arial", 13))
         instructions.place(x=1230, y=100, height=60, width=250)
 
-        newStationCircle = map.create_oval(0, 0, 0, 0, fill='lime')
+        tempCircle = map.create_oval(0, 0, 0, 0, fill='lime')
 
-        map.bind('<Button-1>', lambda event: self.onClick(event, map, newStationCircle))
+        map.bind('<Button-1>', lambda event: self.onClick(event, map, tempCircle))
 
-        confirmButton = Button(mainGUI, text="Add Station", command=lambda: self.writeCoords(self.xyCoords))
+        Label(mainGUI, text="Station Name:", font=("Arial", 13)).place(x=1230, y=230)
+        stnNameTextBox = Entry(mainGUI, textvariable=self.newStationName, font=("Arial", 15),width=20)
+        stnNameTextBox.place(x=1230, y=250, height=35, width=250)
+
+        Label(mainGUI, text="Station Code:", font=("Arial", 13)).place(x=1230, y=300)
+        stnCodeTextBox = Entry(mainGUI, textvariable=self.newStationCode, font=("Arial", 15),width=20)
+        stnCodeTextBox.place(x=1230, y=320, height=35, width=250)
+
+        Label(mainGUI, text="Next Station Code:\n \"-\" if no next station", font=("Arial", 13)).place(x=1230, y=370)
+        nextStnCodeTextBox = Entry(mainGUI, textvariable=self.nextStationCode, font=("Arial", 15),width=20)
+        nextStnCodeTextBox.place(x=1230, y=390, height=35, width=250)
+
+        confirmButton = Button(mainGUI, text="Add Station", command=lambda: self.storeCoords(map, self.xyCoords, self.newStationName, self.newStationCode, self.nextStationCode))
         confirmButton.config(font=("Arial", 30))
-        confirmButton.place(x=1230, y=350, height=50, width=250)
+        confirmButton.place(x=1230, y=450, height=50, width=250)
+
+        abortButton = Button(mainGUI, text="Abort All Changes", command=lambda: self.abortAll())
+        abortButton.config(font=("Arial", 20), fg = 'red')
+        abortButton.place(x=1230, y=650, height=50, width=250)
 
         mainGUI.mainloop()  # Run the GUI
 
@@ -46,14 +68,20 @@ class GUI:
         xyCoords.append(event.y + 7)
 
         LocationCircle = map.coords(LocationCircle, xyCoords[0], xyCoords[1], xyCoords[2], xyCoords[3])
-
+        
         self.xyCoords = xyCoords
-        #self.writeCoords(xyCoords)
 
-    def writeCoords(self, xyCoords):
-        with open('new_station.csv', 'a', newline='', encoding='utf-8') as f:
-            writer = csv.writer(f)
-            writer.writerow(list(xyCoords))
+    def storeCoords(self, map, xyCoords, stnName, stnCode, nextStnCode):
+        # show added station on gui
+        self.newStationCircle[self.newStationCircleCounter] = \
+            map.create_oval(xyCoords[0], xyCoords[1], xyCoords[2], xyCoords[3], fill='MAGENTA')
+        
+        # store the coordinates inside newStations list
+        self.newStations.append([xyCoords[0], xyCoords[1], xyCoords[2], xyCoords[3], stnName.get(), stnCode.get(), nextStnCode.get()])
 
-GUI()
+    def abortAll(self):
+        quit()
+
+addStationApp = GUI()
+print(addStationApp.newStations)
 print("I can do more stuff after GUI")
